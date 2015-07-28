5f9046d53acb43d364fc4d9779a6d24fdb04f053
Ino de Bruijn <ino@ino.pm>
2015-07-28 11:12:37 -0400

    Fix lint checker warnings

--- js/parser.js
+++ js/parser.js
@@ -3,30 +3,30 @@ window.clinicalTimelineParser = (function() {
   function transformToTimelineJSON(clinical_timeline_data) {
     var transformToTrack = function(clin_events) {
       var track = {
-        "label":clin_events[0]["eventType"],
+        "label":clin_events[0].eventType,
         visible:true,
         "times":[]
       };
       clin_events.forEach(function(x) {
         var timepoint = {
-          "starting_time":x["startDate"],
-          "ending_time":x["stopDate"]!== null? x["stopDate"]:x["startDate"],
-          "display":x["stopDate"]!== null? "rect":"circle",
-          tooltip:Object.keys(x["eventData"]).map(function(k) {
-            return [k, x["eventData"][k]]
-          })
+          "starting_time":x.startDate,
+          "ending_time":x.stopDate !== null? x.stopDate:x.startDate,
+          "display":x.stopDate !== null? "rect":"circle",
+          tooltip_tables:[Object.keys(x.eventData).map(function(k) {
+            return [k, x.eventData[k]];
+          })]
         };
-        timepoint.tooltip.push(["START_DATE", x["startDate"]]);
-        if (x["stopDate"] !== null) {
-          timepoint.tooltip.push(["STOP_DATE", x["stopDate"]]);
+        timepoint.tooltip_tables[0].push(["START_DATE", x.startDate]);
+        if (x.stopDate !== null) {
+          timepoint.tooltip_tables[0].push(["STOP_DATE", x.stopDate]);
         }
-        track["times"].push(timepoint);
+        track.times.push(timepoint);
       });
       return track;
-    }
+    };
     var g = _.groupBy(clinical_timeline_data, "eventType");
     var tracks = [];
-    _.each(g, function(x) {tracks.push(transformToTrack(x))});
+    _.each(g, function(x) {tracks.push(transformToTrack(x));});
     return tracks;
   }
 
