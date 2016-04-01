a08faa8b3b98d216095c3071ba4ee77e422a2bbe
Rohan Goel <rohangoel0296@gmail.com>
2016-04-01 14:03:44 +0530

    add travis and screenshot script
    Signed-off-by: Ino de Bruijn <ino@ino.pm>

--- test/make_screenshots.js
+++ test/make_screenshots.js
@@ -1,13 +1,32 @@
-var page = require('webpage').create();
-page.viewportSize = { width: 1000, height: 1000 };
-page.open('../index.html', function(status) {
-    if (status !== 'success') {
-            console.log('Unable to load the address!');
-            phantom.exit(1);
-        } else {
-            window.setTimeout(function () {
-                page.render("screenshots/index_html.png");
-                phantom.exit();
-            }, 50);
-        }
-});
+var system = require('system');
+var args = system.args;
+
+if (args.length !== 4) {
+    console.log("usage: phantomjs make_screenshots.js url screenshot delay");
+    console.log(args);
+    phantom.exit(1);
+}
+
+takeScreenshot = function(url, screenshot, delay) {
+    var page = require('webpage').create();
+    page.viewportSize = { width: 1000, height: 1000 };
+
+    page.onResourceError = function(resourceError) {
+        page.reason = resourceError.errorString;
+        page.reason_url = resourceError.url;
+    };
+
+    page.open(url, function(status) {
+        if (status !== 'success') {
+                console.log('Error opening url ' + page.reason_url + ":" + page.reason);
+                phantom.exit(1);
+            } else {
+                window.setTimeout(function () {
+                    page.render(screenshot);
+                    phantom.exit(0);
+                }, delay);
+            }
+    });
+}
+
+takeScreenshot(args[1], args[2], parseInt(args[3]));
