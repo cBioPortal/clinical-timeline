a16acf79f737ec0779ca4592936c55f586354edc
Rohan Goel <rohangoel0296@gmail.com>
2016-04-08 19:56:48 +0530

    Use separate JSON file for test and index.html
    This moves the JSON data in index.html into a separate test JSON file. Future
    test data should be stored in a similar manner.
    
    Signed-off-by: Ino de Bruijn <ino@ino.pm>

--- test/screenshot_script.sh
+++ test/screenshot_script.sh
@@ -5,11 +5,15 @@ set -e
 # dir of bash script http://stackoverflow.com/questions/59895
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+#read data1
+data1file=${DIR}'/data/data1.json'
+data1=$(cat "$data1file")
+
 # patient view screenshot
 phantomjs --ignore-ssl-errors=true ${DIR}/make_screenshots.js \
-	${DIR}'/../index.html' \
+	${DIR}'/../index.html?json='"$data1" \
 	${DIR}'/screenshots/index_html.png' \
-	50  
+	50
 
 # make sure screenshot is still the same as the one in the repo, if not upload
 # the image
